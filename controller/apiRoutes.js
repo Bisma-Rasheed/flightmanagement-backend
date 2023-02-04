c92b70936ea2657416efb843307addca452b2ea4
Bisma-Rasheed <bismarasheed95@gmail.com>
2023-02-04 22:51:26 +0500

    endpoints added

--- controller/apiRoutes.js
+++ controller/apiRoutes.js
@@ -8,36 +8,36 @@ const userModel = new mongoose.model('Users', userSchema);
 
 var userProfile;
 
-const returnRouter = function(passport){
-    route.get('/', (req, res)=>{
-        res.send({message: 'hello from server'});
+const returnRouter = function (passport) {
+    route.get('/', (req, res) => {
+        res.send({ message: 'hello from server' });
     });
 
-    try{
+    try {
         passport.use(new FacebookStrategy({
             clientID: '1206221456655136',
             clientSecret: '6ac763f1d8999ff6fb301d9b7554c04b',
             callbackURL: "http://localhost:3001/auth/facebook/callback",
             profileFields: ['id', 'displayName', 'email']
         },
-        function(accessToken, refreshToken, profile, done){
-            userProfile=profile;
-            return done(null, userProfile);
-        }
+            function (accessToken, refreshToken, profile, done) {
+                userProfile = profile;
+                return done(null, userProfile);
+            }
         ));
     }
-    catch(err){
+    catch (err) {
         console.log(err);
     }
 
     //facebook auth route
-    route.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' })); //when user logs in through facebook
+    route.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' })); //when user logs in through facebook
 
-    route.get('/auth/facebook/callback', passport.authenticate('facebook', {failureRedirect: '/'}), async (req, res)=>{
-        const userData = await userModel.find({email: userProfile.emails[0].value});
-        if(userData[0]===undefined){
+    route.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/' }), async (req, res) => {
+        const userData = await userModel.find({ email: userProfile.emails[0].value });
+        if (userData[0] === undefined) {
             console.log('I am registering the user through facebook');
-            try{
+            try {
                 const newUser = new userModel({
                     facebookID: userProfile.id,
                     name: userProfile.displayName,
@@ -45,24 +45,42 @@ const returnRouter = function(passport){
                 });
                 const result = await newUser.save();
                 console.log('user added');
-                const email = userProfile.emails[0].value;
-                res.redirect('http://localhost:3000/dashboard/'+email);
+                const fbID = userProfile.id;
+                res.redirect('http://localhost:3000/dashboard/' + fbID);
             }
-            catch(err){
-                res.send({error: err});
+            catch (err) {
+                res.send({ error: err });
             }
         }
         //if exists then login
-        else{
-            const email = userProfile.emails[0].value;
-            res.redirect('http://localhost:3000/dashboard/'+email);
+        else {
+            const fbID = userProfile.id;
+            res.redirect('http://localhost:3000/dashboard/' + fbID);
         }
     });
 
-    route.post('/getuser', async (req, res)=>{
-        const userData = await userModel.find({email: req.body.email});
-        res.send({data: userData[0]});
-    })
+    route.post('/getuser', async (req, res) => {
+        const userData = await userModel.find({ facebookID: req.body.id });
+        res.send({ data: userData[0] });
+    });
+
+    // route.post('/addflight', async (req, res) => {
+    //     var userData = await userModel.find({ email: req.body.email });
+    //     const _id = userData[0]._id;
+    //     const obj = {
+    //         from: req.body.from,
+    //         to: req.body.to,
+    //         dateOfDep: req.body.dep,
+    //         dateOfArrival: req.body.arr,
+    //         flightType: req.body.type
+    //     };
+    //     userData = await userModel.findByIdAndUpdate(
+    //         { _id },
+    //         { $push: { flight: obj } },
+    //         { new: true }
+    //     );
+    //     res.send({ data: userData });
+    // })
 
     return route;
 }
